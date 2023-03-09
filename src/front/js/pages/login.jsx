cb05c53bb9475fb6e8a55060dfa2a5af30b3663a
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-03-09 19:22:46 +0000

    add navigate

--- src/front/js/pages/login.jsx
+++ src/front/js/pages/login.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FormLogin } from "../component/form_login.jsx";
-
+import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
     const navigate = useNavigate();
@@ -8,7 +8,7 @@ export const Login = () => {
         <>
         <h2>Login</h2>
         <FormLogin></FormLogin>
-        <button type="button" class="btn btn-light" onClick={}>
+        <button type="button" class="btn btn-light" onClick={()=>{navigate('/register')}}>
             registrar usuario
         </button>
         </>
