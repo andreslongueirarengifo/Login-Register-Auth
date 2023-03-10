956259e55aad1ee57ab03dc7ef7dadfe9fe012c4
Andres Longueira <andreslongueirarengifo@gmail.com>
2023-03-10 10:39:35 +0000

    Logout sesion

--- src/front/js/pages/private_page.jsx
+++ src/front/js/pages/private_page.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useNavigate } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const PrivatePage = () =>{
+    const { store, actions } = useContext(Context);
     const navigate = useNavigate();
+    const handleClick = (e)=>{
+		localStorage.removeItem("jwt-token");
+		actions.logoutState();
+        navigate('/')
+    }
     return(
     <>
     <div class="container bg-dark">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
             <a class="navbar-brand text-white" href="#">Private Page</a>
-            <button type="button" class="btn btn-danger" onClick={()=>{navigate('/')}}>Cerrar sesión</button>
+            <button type="button" class="btn btn-danger" onClick={handleClick}>Cerrar sesión</button>
         </div>
     </nav>
     </div>
